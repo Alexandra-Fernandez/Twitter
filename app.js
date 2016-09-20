5eb8fcf791c9fe740300a430a3b53fced99e8758
Alexandra <alexandrafernandezquispe@gmail.com>
2016-09-20 16:46:19 -0500

    subiendo2

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 window.addEventListener("load", function() {
-	var boton=document.getElementById("click");
-	var textArea="";
-	boton.addEventListener("click",function(){
-		textArea= document.getElementById("input").value;
-		
+	var boton=document.getElementById("boton");
+    var textArea = document.getElementById("input");
+	boton.addEventListener("click",function(e){
+        e.preventDefault();
+        var textArea = document.getElementById("input").value;
    		
-   		var newItem = document.createElement("p");
-    	var textnode = document.createTextNode(textArea);
-    	newItem.appendChild(textnode);
-
+   		var newItem = document.createElement("div");
+        newItem.innerText = textArea;
+    
     	var contenedor = document.getElementById("contenedor");
     	contenedor.insertBefore(newItem, contenedor.childNodes[0]);
-
+        textArea.value = "";
+        
 	});
+    function agregaMensaje () {
+        var div = document.createElement("div");
+        div.innerText = texto;
+        var mensajes = document.getElementById("contenedor");
+        mensajes.inserBefore(div, mensajes.childNodes[0]);
+    };
+    
+    textArea.addEventListener("keydown",function(){
+       boton.disabled=false;
+       var limite = 140;
+       var longitud = document.getElementById("input").value.length;
+       var contador = document.getElementById("contador");
+       contador.innerText= limite-longitud;
+       if (longitud>=limite){
+           contador.classList.add("colorRed");
+           boton.disabled=true;
 
+       } else {
+           contador.classList.remove("colorRed");
+       }
+    });       
 });
+
+    
+    
+ 
